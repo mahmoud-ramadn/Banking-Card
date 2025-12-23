5530c20818803c0103dd1e7cf3eac6f8b7e75451
mahmoud-ramadn <ahmedshawareb567@gmail.com>
2025-12-24 00:18:32 +0200

    feat:update-chore

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,14 +3,15 @@ import { GridScan } from './GridScan';
 export default function Hero() {
   return (
     <div className="relative mt-20">
-      <div style={{ width: '100%' }} className="relative h-[600px] md:h-[900px]">
+      <div style={{ width: '100%' }} className="relative h-[600px] md:h-[900px] rounded-lg overflow-hidden">
         <GridScan
           sensitivity={2}
           lineThickness={1}
-          scanGlow={0.1}
-          linesColor="#0dafc5"
-          gridScale={0.02}
-          scanColor="#1889b9"
+          scanGlow={2}
+          linesColor="#392e4e"
+          gridScale={0.5}
+          scanColor="#ff9ffc"
+          lineJitter={1}
           scanOpacity={0.4}
           enablePost
           bloomIntensity={0.6}
